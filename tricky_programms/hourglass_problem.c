2b2cf8803b4a2912629b9d9a43c5b26c9609f65d
prashant <prashantchikhalkar@gmail.com>
2016-08-16 16:54:29 +0530

    Update hourglass_problem.c

--- tricky_programms/hourglass_problem.c
+++ tricky_programms/hourglass_problem.c
@@ -4,3 +4,34 @@
  *
  *
  **/
+
+#include <stdio.h>
+
+#define MAX_SIZE  6
+
+int main (void)
+{
+	int arr [MAX_SIZE] [MAX_SIZE];
+	int row, col;
+	int sum, maxsum;
+	
+	printf ("Enter elements of an array :\n");
+	for (row = 0; row < MAX_SIZE; row++) {
+		for (col = 0; col < MAX_SIZE; col++) {
+			scanf ("%d", &arr [row] [col]);
+		}
+	}
+  	for (row = 0; row < 6; row++) {
+  		for (col = 0; col < 6; col++) {
+  			if (col + 2 < 6 && row + 2 < 6) {
+  				sum = arr [row] [col] + arr [row] [col + 1] + arr [row] [col + 2]
+  					+ arr [row + 1] [col + 1]
+  					+ arr [row + 2] [col] + arr [row + 2] [col + 1] + arr [row + 2] [col + 2];
+  				if (sum > maxsum)
+  					maxsum = sum;
+  			}
+  		}
+  	}
+  	printf ("%d\n", maxsum);
+  	return 0;
+}
