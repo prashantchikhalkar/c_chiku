e12f7af6dc8f1dd808f55cb890ce15c1e74002c4
PRASHANT CHIKHALKAR <prashantchikhalkar@gmail.com>
2016-08-21 13:56:06 +0530

    atoi.c - added negative value support

--- tricky_programms/atoi.c
+++ tricky_programms/atoi.c
@@ -12,18 +12,27 @@ int myAtoi (char *str);
 
 int myAtoi (char *str)
 {
-	int res = 0;
-	int i;
+	if (*str == NULL)
+		return 0;
 
-	for (i = 0; str [i] != '\0'; ++i)
+	int res = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (str [0] == '-') {
+		sign = -1;
+		i++;
+	}
+	for (; str [i] != '\0'; ++i)
 		res = res * 10 + str [i] - '0';
 
-	return res;	
+	return sign * res;	
 }
 
 int main (void)
 {
-	char str [SIZE] = "123459876";
+	char str [SIZE] = "-123";
+	//char str [SIZE] = "\0";
 	int val = myAtoi (str);
 	printf ("%d ", val);
 	printf ("\n");
