883f41a90044228ab7eb8e1eef2ab2856485a161
prashant <prashantchikhalkar@gmail.com>
2016-10-25 12:45:08 +0530

    Create my_strcpy.c

---
+++ tricky_programms/my_strcpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void my_strcpy (char *dst, const char *src)
+{
+    while (*src != '\0')
+    {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+}
+
+int main (void)
+{
+    char *src = "HELLO";
+    char dst [10] = {0};
+    
+    printf ("Source string is = %s\n", src);
+    printf ("Destination string before copying is = %s\n", dst);
+    
+    my_strcpy (dst, src);
+    
+    printf ("Destination string after copying = %s\n", dst);
+    
+    return 0;
+    
+}
