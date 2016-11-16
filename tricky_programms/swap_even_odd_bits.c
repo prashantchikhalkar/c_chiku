1fd9065ff956fa54c90a97607197800723f45ea0
prashant <prashantchikhalkar@gmail.com>
2016-11-16 12:31:14 +0530

    Update swap_even_odd_bits.c

--- tricky_programms/swap_even_odd_bits.c
+++ tricky_programms/swap_even_odd_bits.c
@@ -1,11 +1,19 @@
+/**
+ *  @file       swap_even_odd_bits.c
+ *  @brief      C program to swap Even bits with corresponding Odd bits in given 32 nit integer.
+ *  @author     Prashant Chikhalkar
+ *  @date       16th November 2016
+ *  
+ **/
+ 
 #include <stdio.h>
 
 unsigned int swapbits (unsigned int num)
 {
-    // extract all even bits from given number
+    // extract all even bits from given number, mask with A = 1010
     unsigned int even_bits = num & 0xAAAAAAAA;
     
-    // extract all even bits from given number
+    // extract all even bits from given number, mask with 5 = 0101
     unsigned int odd_bits = num & 0x55555555;
     
     // right shift even bits
